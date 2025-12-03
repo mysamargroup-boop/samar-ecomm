7df4f439881aaa0be8c47c3e8ec9f5eea0c0d758
Shubham Nema <msnema7@gmail.com>
2025-12-03 19:53:04 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
-      <body className={cn('font-body antialiased flex flex-col min-h-screen', manrope.variable)}>
+      <body className={cn('font-body antialiased flex flex-col min-h-screen', manrope.variable)} suppressHydrationWarning>
         <AppHeader />
         <main className="flex-grow">{children}</main>
         <Footer />
