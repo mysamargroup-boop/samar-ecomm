9b47c4a2f978b81501ce43dec69d33678e4144e3
Shubham Nema <msnema7@gmail.com>
2025-12-03 19:00:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1200999547-6f76b",
+  "appId": "1:840402456466:web:face6dcc61c452700a7b4b",
+  "apiKey": "AIzaSyDGn5rIJ04U62tZlL8pKEG97QVrGsSp97M",
+  "authDomain": "studio-1200999547-6f76b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "840402456466"
+};
