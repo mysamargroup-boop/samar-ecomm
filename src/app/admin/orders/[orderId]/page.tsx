3ff5165715bc962cecd7e0d1b119836b0b1a9fec
Shubham Nema <msnema7@gmail.com>
2025-12-03 21:08:48 +0000

    still error

--- src/app/admin/orders/[orderId]/page.tsx
+++ src/app/admin/orders/[orderId]/page.tsx
@@ -18,8 +18,10 @@ export default function OrderDetailsPage({ params }: { params: { orderId: string
         <div className="flex items-center gap-4">
             <Button variant="outline" size="icon" asChild>
                 <Link href="/admin/orders">
+                  <>
                     <ChevronLeft className="h-4 w-4" />
                     <span className="sr-only">Back to Orders</span>
+                  </>
                 </Link>
             </Button>
             <h1 className="text-2xl font-bold font-headline">Order #{order.id.split('_')[1]}</h1>
