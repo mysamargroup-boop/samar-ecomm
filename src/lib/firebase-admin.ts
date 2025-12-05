0c6c57460d27bb6fab27217f292cf655b4672763
Shubham Nema <msnema7@gmail.com>
2025-12-05 06:51:28 +0000

    aisa aa raha hai

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,5 +1,9 @@
 
 import * as admin from 'firebase-admin';
+import { config } from 'dotenv';
+
+// Load environment variables from .env file
+config();
 
 // This is a server-only file. It should not be imported into client-side code.
 
