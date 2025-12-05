5a63fb861823041812dfe17f2c919a57d5818d80
Shubham Nema <msnema7@gmail.com>
2025-12-05 10:57:49 +0000

    i have created a realtime database in firebase and the url is : https://

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,5 +5,6 @@ export const firebaseConfig = {
   "apiKey": "AIzaSyDGn5rIJ04U62tZlL8pKEG97QVrGsSp97M",
   "authDomain": "studio-1200999547-6f76b.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "840402456466"
+  "messagingSenderId": "840402456466",
+  "databaseURL": "https://studio-1200999547-6f76b-default-rtdb.firebaseio.com/"
 };
